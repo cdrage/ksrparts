40f13e4c5c1b89415ad07fc9738248d19767e999
Charlie Drage <charlie@charliedrage.com>
2021-01-07 13:11:56 -0500

    add gallery

---
+++ generate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rm gallery.html
+
+cat gallery1.html.template >> gallery.html
+
+i=1
+for f in assets/images/tracks/*.png; do
+  name=$(echo $f | cut -f 1 -d '.')
+  base="$(basename -- "$name")"
+  echo $name
+  nospace=$( printf "%s\n" "$f" | sed 's/ /%20/g' )
+
+  cat >> gallery.html << EOL
+              <div class="col-md-3">
+                <div class="table-left">
+                  <div class="track">$base</div>
+                  <img class="bike-fluid" src="$nospace">
+                </div>
+              </div>
+EOL
+  i=$((i+1))
+  if (( $i == 5 )); then
+    i=1
+    echo '</div><div class="row">' >> gallery.html
+  fi
+done
+
+cat gallery2.html.template >> gallery.html
+
+perl -p -i -e "s/\r//g" gallery.html
