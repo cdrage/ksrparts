5beaa91e2d93da60c01d09035ae2a7e93ec10909
Charlie Drage <charlie@charliedrage.com>
2021-04-27 08:58:13 -0400

    update
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- generate.sh
+++ generate.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+
+
+#
+# GALLERY GENERATION
+#
 rm gallery.html
-
-cat gallery1.html.template >> gallery.html
-
+cat templates/gallery1.html.template >> gallery.html
 i=1
 for f in assets/images/tracks/*.png; do
   name=$(echo $f | cut -f 1 -d '.')
@@ -25,7 +28,7 @@ EOL
     echo '</div><div class="row trackrow text-center">' >> gallery.html
   fi
 done
-
-cat gallery2.html.template >> gallery.html
-
+cat templates/gallery2.html.template >> gallery.html
 perl -p -i -e "s/\r//g" gallery.html
+
+
