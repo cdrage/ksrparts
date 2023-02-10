bec64fb648edc2d5f4ae32820263a0f0aa823c50
Charlie Drage <charlie@charliedrage.com>
2023-02-10 09:37:21 -0500

    update
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- generate.sh
+++ generate.sh
@@ -7,11 +7,7 @@ sed-replace() {
 
 INDEX_TITLE="Motorcycle Parts and Art"
 LAPTIMERS_TITLE="GPS Lap Timers"
-TRACKMAPS_TITLE="Race track Sculptures"
-LEVERGUARDS_TITLE="Brake and Clutch Lever Guards"
-TRACKMAPS_ORDER_LINK="https://forms.gle/Zmidy3HAFgYoa4Vd9"
 LAPTIMERS_ORDER_LINK="https://forms.gle/EfyEvYZq5Va6bMiQ8"
-LEVERGUARDS_ORDER_LINK="https://forms.gle/NfcwoFzS9zZkn2Yb7"
 
 #########
 # INDEX #
@@ -22,59 +18,4 @@ cat templates/header.html.template \
   templates/footer.html.template \
   >> index.html
 sed-replace %TITLE% "$INDEX_TITLE" index.html
-sed-replace %TRACKMAPS_ORDER_LINK% "$TRACKMAPS_ORDER_LINK" index.html
 sed-replace %LAPTIMERS_ORDER_LINK% "$LAPTIMERS_ORDER_LINK" index.html
-sed-replace %LEVERGUARDS_ORDER_LINK% "$LEVERGUARDS_ORDER_LINK" index.html
-
-#############
-# LAPTIMERS #
-#############
-rm laptimers.html
-cat templates/header.html.template \
-  templates/laptimers.html.template \
-  templates/footer.html.template \
-  >> laptimers.html
-sed-replace %TITLE% "$LAPTIMERS_TITLE" laptimers.html
-sed-replace %LAPTIMERS_ORDER_LINK% "$LAPTIMERS_ORDER_LINK" laptimers.html
-
-######################
-# TRACKMAPS GENERATION #
-######################
-rm trackmaps.html
-
-# Insert header
-cat templates/header.html.template >> trackmaps.html
-
-# Generate template
-cat templates/trackmaps1.html.template >> trackmaps.html
-i=1
-for f in assets/images/tracks/*.png; do
-  name=$(echo $f | cut -f 1 -d '.')
-  base="$(basename -- "$name")"
-  echo $name
-  nospace=$( printf "%s\n" "$f" | sed 's/ /%20/g' )
-
-  cat >> trackmaps.html << EOL
-              <div class="col-md-2">
-                <div class="table-left">
-                  <img class="bike-fluid" src="$nospace">
-                  <div class="track">$base</div>
-                </div>
-              </div>
-EOL
-  i=$((i+1))
-  if (( $i == 7 )); then
-    i=1
-    echo '</div><div class="row trackrow text-center">' >> trackmaps.html
-  fi
-done
-cat templates/trackmaps2.html.template >> trackmaps.html
-
-# Insert footer
-cat templates/footer.html.template >> trackmaps.html
-
-perl -p -i -e "s/\r//g" trackmaps.html
-
-# VARIABLE REPLACEMENTS
-sed-replace %TRACKMAPS_ORDER_LINK% "$TRACKMAPS_ORDER_LINK" trackmaps.html
-sed-replace %TITLE% "$TRACKMAPS_TITLE" trackmaps.html
